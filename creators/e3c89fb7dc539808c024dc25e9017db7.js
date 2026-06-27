30eaf984d418dc1b9f18d4a3882a8e3817d19864
Jayden Shaw <jaydensw93@users.noreply.github.com>
2026-06-27 17:21:03 -0400

    auto-refresh swipe file 2026-06-27 17:21

--- creators/e3c89fb7dc539808c024dc25e9017db7.js
+++ creators/e3c89fb7dc539808c024dc25e9017db7.js
@@ -1,2 +1,2 @@
-window.CREATOR_META={"iters": 150000, "generated_at": "2026-06-27 20:53 UTC", "name": "Eric"};
-window.CREATOR_ENC="nF6d1qWq3ymOMz0zm1WLvo9wPjFJniDtbxHEUVLr3KcAsqrqUJlZpWHSn2wJdL1+67iMsJJuEmYZ2Iv25QUbnOB1xhtLMX5dB8Xw94bZ9XqDISrPrb/JGM477xVb3DfyPXJPDYObh8S2CgaThSA0pNdQCD6fFcI9PX1ZVnbZuRnpFef1YzfecC9ZoueVfUrkcMlNCCK2leEOTmfcW7H5fxU3nG8=";
+window.CREATOR_META={"iters": 150000, "generated_at": "2026-06-27 21:21 UTC", "name": "Eric"};
+window.CREATOR_ENC="eYDHOpBq3dTrP8nGH3n+kV2GmJq3Mqc7+3aj/FCze9Hu7rOUPP6EKsJtm3af4k1zzOh2lrODageEmFXtKSdVk5jplmq20DTs6mhNjS50DUhkOe0CTLSGPZO7cSpze2r4qP/H9Z9ciaKcYINZrEVuFbC2eolLTlrGxHdX9txjkThBg9Oubyth7/nJmHA9idG7hO933dRLmRmzb34zulPZaQkpgO0=";
